8d8fbc2a1699fd9960c546583965ce58a0d12183
Mostafa5hattab <104473913+Mostafa5hattab@users.noreply.github.com>
2022-05-02 01:05:21 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+let img = document.getElementById("img");
+let boody =document.getElementById("boody");
+function phon(ph){
+    img.src=ph;
+}
+
+function collor(c){
+    boody.style.background=c;
+}
